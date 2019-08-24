2d8eb0593323d3fdefba6c77eae6406a61e24ac6
Loice Meyo <loicemeyo@gmail.com>
2019-08-24 14:28:30 +0300

    the_census: Setup database, models, migrations and validations

---
+++ database/models/location.js
@@ -0,0 +1,29 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Location = sequelize.define('Location', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.STRING,
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    females: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    },
+    males: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    },
+  }, {
+    tableName: 'locations'
+  });
+  Location.associate = (models) => {
+    Location.hasMany(Location, {as: 'Sublocations', foreignKey: 'parentLocation', hooks: true})
+  };
+  return Location;
+};
